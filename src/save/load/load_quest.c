6f5f46b9b0055fc68b704d1098f78babecd47666
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-08 20:31:18 +0200

    🐛 fix(load_quest.c): fix boolean conversion of has_armor variable

--- src/save/load/load_quest.c
+++ src/save/load/load_quest.c
@@ -9,12 +9,13 @@
 
 bool load_quest(var_t *var, char *line)
 {
+    bool has_armor_bool = false;
     char *has_armor_str = NULL;
 
     if (my_strstr(line, "has_armor: ")) {
         has_armor_str = line + 11;
-        int has_armor_int = my_getnbr(has_armor_str);
-        if (has_armor_int == 1) {
+        has_armor_bool = my_getnbr(has_armor_str);
+        if (has_armor_bool) {
             sfSprite_setTexture(var->mc->sprite, var->armor, sfTrue);
             var->mc->rect.top = 0;
             var->mc->rect.width = 77;
@@ -23,7 +24,6 @@ bool load_quest(var_t *var, char *line)
             sfSprite_setTextureRect(var->mc->sprite, var->mc->rect);
             var->has_armor = true;
         }
-        free(has_armor_str);
         return true;
     }
     return false;
