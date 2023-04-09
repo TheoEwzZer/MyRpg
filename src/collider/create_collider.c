54d46353dace30f7355324bca838576f00038680
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-09 23:48:42 +0200

    🔨 refactor: replace var->collider with COLLIDER macro 🔨 refactor: replace var->collider_bounds with COLLIDER_BOUNDS macro 🔨 refactor: use float values for sfVector2f positions in create_collider() function

--- src/collider/create_collider.c
+++ src/collider/create_collider.c
@@ -10,11 +10,11 @@
 void create_collider(var_t *var)
 {
     var->hitbox = sfRectangleShape_create();
-    sfRectangleShape_setPosition(var->hitbox, (sfVector2f){1700, 1340});
-    var->collider = malloc(sizeof(sfRectangleShape *) * 65);
+    sfRectangleShape_setPosition(var->hitbox, (sfVector2f){1700.0f, 1340.0f});
+    COLLIDER = malloc(sizeof(sfRectangleShape *) * 65);
     var->collider_bounds = malloc(sizeof(sfFloatRect) * 65);
     for (unsigned int i = 0; i < 65; i++)
-        var->collider[i] = sfRectangleShape_create();
+        COLLIDER[i] = sfRectangleShape_create();
     create_water_collider(var);
     create_north_west_position(var);
     create_north_west_size(var);
@@ -26,5 +26,5 @@ void create_collider(var_t *var)
     create_png_collider(var);
     create_foreground(var);
     for (unsigned int i = 0; i < 65; i++)
-        var->collider_bounds[i] = GET_BOUNDS(var->collider[i]);
+        var->collider_bounds[i] = GET_BOUNDS(COLLIDER[i]);
 }
