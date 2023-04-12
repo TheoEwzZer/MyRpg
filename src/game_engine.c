dc676fda66d6f873e319a04709caefcf1b2f88de
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-12 16:32:45 +0200

    🐛 fix: use macro

--- src/game_engine.c
+++ src/game_engine.c
@@ -12,15 +12,14 @@ void game_engine(var_t *var)
     sfRenderWindow_clear(var->window, sfBlack);
     sfRenderWindow_setView(var->window, var->view);
     DRAW_SPRITE(var->background_sprite);
-    if (var->mc->attack)
+    if (PLAYER->attack)
         attack_move(var);
     blacksmith_move(var);
     girl_move(var);
     pnj_move(var);
     if (var->quest_progress == ENEMIES)
         check_enemies(var);
-    DRAW_SPRITE(var->mc->sprite);
-    DRAW_RECTANGLE(var->hitbox);
+    DRAW_SPRITE(PLAYER->sprite);
     for (unsigned int i = 0; i < 9; i++)
         DRAW_SPRITE(var->foreground[i]);
     var->frame_count++;
