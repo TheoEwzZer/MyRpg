fa3c0e9cf6ffbfe8109021ed7ed13526dcc25426
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-14 13:52:15 +0200

    ✨ feat: save skeleton and orc life to file

--- src/save/save/save_quest.c
+++ src/save/save/save_quest.c
@@ -9,7 +9,19 @@
 
 void save_quest(var_t *var, FILE *file)
 {
+    size_t n = 0;
+    char *str = NULL;
+
     fwrite("quest_progress: ", sizeof(char), 16, file);
     fwrite(int_to_str(var->quest_progress, NULL), sizeof(char), 1, file);
     fwrite("\n", sizeof(char), 1, file);
+    fwrite("skeleton_life: ", sizeof(char), 15, file);
+    str = int_to_str(var->skeleton->life, &n);
+    fwrite(str, sizeof(char), n, file);
+    fwrite("\n", sizeof(char), 1, file);
+    fwrite("orc_life: ", sizeof(char), 10, file);
+    str = int_to_str(var->orc->life, &n);
+    fwrite(str, sizeof(char), n, file);
+    fwrite("\n", sizeof(char), 1, file);
+    free(str);
 }
