8387981099e2e3ab80a599d689d8a55ff0c8bca5
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-14 13:50:18 +0200

    🎨 style: add inventory sprite and display it when inventory is open 🎨 style: set inventory sprite texture and scale in init_ui() function 🎨 style: set inventory sprite position in display_ui() function

--- src/ui.c
+++ src/ui.c
@@ -11,6 +11,7 @@ void display_ui(var_t *var)
 {
     sfVector2f offset = sfView_getCenter(var->view);
     sfVector2f position_text = {0.0f, 0.0f};
+    sfVector2f inventory = {0.0f, 0.0f};
 
     offset.x -= sfView_getSize(var->view).x / 2.0f;
     offset.y -= sfView_getSize(var->view).y / 2.0f;
@@ -20,11 +21,18 @@ void display_ui(var_t *var)
     sfText_setPosition(var->quest_text->text, position_text);
     DRAW_SPRITE(var->quest_text->box);
     sfRenderWindow_drawText(var->window, var->quest_text->text, NULL);
+
+    inventory.x = offset.x + 235.0f;
+    inventory.y = offset.y + 75.0f;
+    sfSprite_setPosition(INVENTORY->sprite, inventory);
+    if (INVENTORY->is_open)
+        DRAW_SPRITE(INVENTORY->sprite);
 }
 
 void init_ui(var_t *var)
 {
     sfTexture *box_texture = CREATE_FROM_FILE("assets/box/quest_box.png");
+    INVENTORY->texture = CREATE_FROM_FILE("assets/player/inventory.png");
 
     var->quest_text->text = sfText_create();
     sfText_setString(var->quest_text->text, "Talk to Priscilla");
@@ -38,4 +46,7 @@ void init_ui(var_t *var)
     var->quest_text->box = sfSprite_create();
     sfSprite_setScale(var->quest_text->box, (sfVector2f){0.05f, 0.05f});
     sfSprite_setTexture(var->quest_text->box, box_texture, sfTrue);
+    INVENTORY->sprite = sfSprite_create();
+    sfSprite_setTexture(INVENTORY->sprite, INVENTORY->texture, sfTrue);
+    sfSprite_setScale(INVENTORY->sprite, (sfVector2f){1.5f, 1.5f});
 }
