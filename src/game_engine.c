3d5335327d5395822aa932aad7d8521ffe948541
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-16 18:51:53 +0200

    🐛 fix: rename game_engine function to game_engine2 and add new game_engine function that takes an sfEvent parameter ✨ feat: add load_game_and_engine function that loads a game from a save file and starts the game engine

--- src/game_engine.c
+++ src/game_engine.c
@@ -15,7 +15,13 @@ void check_quest(var_t *var)
         DRAW_SPRITE(var->foreground[9]);
 }
 
-void game_engine(var_t *var)
+void load_game_and_engine(var_t *var, sfEvent event)
+{
+    load_game("save.txt", var);
+    game_engine(var, event);
+}
+
+void game_engine2(var_t *var)
 {
     sfRenderWindow_clear(var->window, sfBlack);
     sfRenderWindow_setView(var->window, var->view);
@@ -27,7 +33,7 @@ void game_engine(var_t *var)
     pnj_move(var);
     check_quest(var);
     DRAW_SPRITE(PLAYER->sprite);
-    for (unsigned int i = 0; i < 9; i++)
+    for (sfUint32 i = 0; i < 9; i++)
         DRAW_SPRITE(var->foreground[i]);
     var->frame_count++;
     move_leaves(var);
@@ -36,4 +42,13 @@ void game_engine(var_t *var)
     display_ui(var);
     display_life(var);
     sfRenderWindow_display(var->window);
+}
+
+void game_engine(var_t *var, sfEvent event)
+{
+    while (sfRenderWindow_isOpen(var->window)) {
+        if (sfRenderWindow_pollEvent(var->window, &event))
+            check_event(var, event);
+        game_engine2(var);
+    }
 }
