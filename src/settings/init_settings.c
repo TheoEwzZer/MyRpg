0e7be547ab96800e1b58e5bc562d1858f23b2e49
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-17 16:46:55 +0200

    🔨 refactor: rename variables and functions

--- src/settings/init_settings.c
+++ src/settings/init_settings.c
@@ -9,91 +9,91 @@
 
 settings_t *init_settings(void)
 {
-    settings_t *menuv = malloc(sizeof(settings_t));
+    settings_t *menu = malloc(sizeof(settings_t));
     sfTexture *music_texture = CREATE_FROM_FILE("assets/settings/music.png");
     sfTexture *size_texture = CREATE_FROM_FILE("assets/settings/size.png");
     sfTexture *sound_texture = CREATE_FROM_FILE("assets/settings/sound.png");
     sfTexture *texture = CREATE_FROM_FILE("assets/settings/background.jpg");
 
-    menuv->menusprite = sfSprite_create();
-    sfSprite_setTexture(menuv->menusprite, texture, sfTrue);
-    menuv->musicsprite = sfSprite_create();
-    sfSprite_setTexture(menuv->musicsprite, music_texture, sfTrue);
-    sfSprite_setPosition(menuv->musicsprite, (sfVector2f){50, 150});
-    menuv->soundsprite = sfSprite_create();
-    sfSprite_setTexture(menuv->soundsprite, sound_texture, sfTrue);
-    sfSprite_setPosition(menuv->soundsprite, (sfVector2f){600, 150});
-    menuv->sizesprite = sfSprite_create();
-    sfSprite_setTexture(menuv->sizesprite, size_texture, sfTrue);
-    sfSprite_setPosition(menuv->sizesprite, (sfVector2f){50, 600});
-    init_settingstwo(menuv);
-    return menuv;
+    menu->menu_sprite = sfSprite_create();
+    sfSprite_setTexture(menu->menu_sprite, texture, sfTrue);
+    menu->music_sprite = sfSprite_create();
+    sfSprite_setTexture(menu->music_sprite, music_texture, sfTrue);
+    sfSprite_setPosition(menu->music_sprite, (sfVector2f){50, 150});
+    menu->sound_sprite = sfSprite_create();
+    sfSprite_setTexture(menu->sound_sprite, sound_texture, sfTrue);
+    sfSprite_setPosition(menu->sound_sprite, (sfVector2f){600, 150});
+    menu->size_sprite = sfSprite_create();
+    sfSprite_setTexture(menu->size_sprite, size_texture, sfTrue);
+    sfSprite_setPosition(menu->size_sprite, (sfVector2f){50, 600});
+    init_settings_two(menu);
+    return menu;
 }
 
-void init_settingstwo(settings_t *menuv)
+void init_settings_two(settings_t *menu)
 {
-    sfTexture *jauge0_texture = CREATE_FROM_FILE("assets/settings/jauge0.png");
-    sfTexture *jauge1_texture = CREATE_FROM_FILE("assets/settings/jauge1.png");
+    sfTexture *gauge0_texture = CREATE_FROM_FILE("assets/settings/gauge0.png");
+    sfTexture *gauge1_texture = CREATE_FROM_FILE("assets/settings/gauge1.png");
     sfTexture *resolution_texture = CREATE_FROM_FILE("assets/settings/res.png");
 
-    menuv->resolutionsprite = sfSprite_create();
-    sfSprite_setTexture(menuv->resolutionsprite, resolution_texture, sfTrue);
-    sfSprite_setPosition(menuv->resolutionsprite, (sfVector2f){600, 600});
-    menuv->jaugem = 2;
-    menuv->jauges = 2;
-    menuv->jaugem0 = sfSprite_create(); menuv->jauges0 = sfSprite_create();
-    sfSprite_setTexture(menuv->jaugem0, jauge0_texture, sfTrue);
-    sfSprite_setPosition(menuv->jaugem0, (sfVector2f){100, 400});
-    sfSprite_setTexture(menuv->jauges0, jauge0_texture, sfTrue);
-    sfSprite_setPosition(menuv->jauges0, (sfVector2f){650, 400});
-    menuv->jaugem1 = sfSprite_create(); menuv->jauges1 = sfSprite_create();
-    sfSprite_setTexture(menuv->jaugem1, jauge1_texture, sfTrue);
-    sfSprite_setPosition(menuv->jaugem1, (sfVector2f){100, 400});
-    sfSprite_setTexture(menuv->jauges1, jauge1_texture, sfTrue);
-    sfSprite_setPosition(menuv->jauges1, (sfVector2f){650, 400});
-    init_settingsthree(menuv);
+    menu->resolution_sprite = sfSprite_create();
+    sfSprite_setTexture(menu->resolution_sprite, resolution_texture, sfTrue);
+    sfSprite_setPosition(menu->resolution_sprite, (sfVector2f){600, 600});
+    menu->gauge_m = 2;
+    menu->gauge_s = 2;
+    menu->gauge_m0 = sfSprite_create(); menu->gauge_s0 = sfSprite_create();
+    sfSprite_setTexture(menu->gauge_m0, gauge0_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_m0, (sfVector2f){100, 400});
+    sfSprite_setTexture(menu->gauge_s0, gauge0_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_s0, (sfVector2f){650, 400});
+    menu->gauge_m1 = sfSprite_create(); menu->gauge_s1 = sfSprite_create();
+    sfSprite_setTexture(menu->gauge_m1, gauge1_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_m1, (sfVector2f){100, 400});
+    sfSprite_setTexture(menu->gauge_s1, gauge1_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_s1, (sfVector2f){650, 400});
+    init_settings_three(menu);
 }
 
-void init_settingsthree(settings_t *menuv)
+void init_settings_three(settings_t *menu)
 {
-    sfTexture *jauge2_texture = CREATE_FROM_FILE("assets/settings/jauge2.png");
-    sfTexture *jauge3_texture = CREATE_FROM_FILE("assets/settings/jauge3.png");
-    sfTexture *jauge4_texture = CREATE_FROM_FILE("assets/settings/jauge4.png");
+    sfTexture *gauge2_texture = CREATE_FROM_FILE("assets/settings/gauge2.png");
+    sfTexture *gauge3_texture = CREATE_FROM_FILE("assets/settings/gauge3.png");
+    sfTexture *gauge4_texture = CREATE_FROM_FILE("assets/settings/gauge4.png");
 
-    menuv->jaugem2 = sfSprite_create(); menuv->jauges2 = sfSprite_create();
-    sfSprite_setTexture(menuv->jaugem2, jauge2_texture, sfTrue);
-    sfSprite_setPosition(menuv->jaugem2, (sfVector2f){100, 400});
-    sfSprite_setTexture(menuv->jauges2, jauge2_texture, sfTrue);
-    sfSprite_setPosition(menuv->jauges2, (sfVector2f){650, 400});
-    menuv->jaugem3 = sfSprite_create(); menuv->jauges3 = sfSprite_create();
-    sfSprite_setTexture(menuv->jaugem3, jauge3_texture, sfTrue);
-    sfSprite_setPosition(menuv->jaugem3, (sfVector2f){100, 400});
-    sfSprite_setTexture(menuv->jauges3, jauge3_texture, sfTrue);
-    sfSprite_setPosition(menuv->jauges3, (sfVector2f){650, 400});
-    menuv->jaugem4 = sfSprite_create(); menuv->jauges4 = sfSprite_create();
-    sfSprite_setTexture(menuv->jaugem4, jauge4_texture, sfTrue);
-    sfSprite_setPosition(menuv->jaugem4, (sfVector2f){100, 400});
-    sfSprite_setTexture(menuv->jauges4, jauge4_texture, sfTrue);
-    sfSprite_setPosition(menuv->jauges4, (sfVector2f){650, 400});
-    init_mute(menuv);
+    menu->gauge_m2 = sfSprite_create(); menu->gauge_s2 = sfSprite_create();
+    sfSprite_setTexture(menu->gauge_m2, gauge2_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_m2, (sfVector2f){100, 400});
+    sfSprite_setTexture(menu->gauge_s2, gauge2_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_s2, (sfVector2f){650, 400});
+    menu->gauge_m3 = sfSprite_create(); menu->gauge_s3 = sfSprite_create();
+    sfSprite_setTexture(menu->gauge_m3, gauge3_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_m3, (sfVector2f){100, 400});
+    sfSprite_setTexture(menu->gauge_s3, gauge3_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_s3, (sfVector2f){650, 400});
+    menu->gauge_m4 = sfSprite_create(); menu->gauge_s4 = sfSprite_create();
+    sfSprite_setTexture(menu->gauge_m4, gauge4_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_m4, (sfVector2f){100, 400});
+    sfSprite_setTexture(menu->gauge_s4, gauge4_texture, sfTrue);
+    sfSprite_setPosition(menu->gauge_s4, (sfVector2f){650, 400});
+    init_mute(menu);
 }
 
-void init_mute(settings_t *menuv)
+void init_mute(settings_t *menu)
 {
     sfTexture *mute_texture = CREATE_FROM_FILE("assets/settings/mute.png");
-    sfVector2f mutem_pos = {50, 400};
-    sfVector2f mutes_pos = {600, 400};
+    sfVector2f mute_music_pos = {50, 400};
+    sfVector2f mute_sound_pos = {600, 400};
 
-    menuv->mutem = sfSprite_create();
-    sfSprite_setTexture(menuv->mutem, mute_texture, sfTrue);
-    sfSprite_setPosition(menuv->mutem, mutem_pos);
-    menuv->mutes = sfSprite_create();
-    sfSprite_setTexture(menuv->mutes, mute_texture, sfTrue);
-    sfSprite_setPosition(menuv->mutes, mutes_pos);
-    init_resolution(menuv);
+    menu->mute_music = sfSprite_create();
+    sfSprite_setTexture(menu->mute_music, mute_texture, sfTrue);
+    sfSprite_setPosition(menu->mute_music, mute_music_pos);
+    menu->mute_sound = sfSprite_create();
+    sfSprite_setTexture(menu->mute_sound, mute_texture, sfTrue);
+    sfSprite_setPosition(menu->mute_sound, mute_sound_pos);
+    init_resolution(menu);
 }
 
-void init_resolution(settings_t *menuv)
+void init_resolution(settings_t *menu)
 {
     sfTexture *res1_texture = CREATE_FROM_FILE("assets/settings/800.png");
     sfTexture *res2_texture = CREATE_FROM_FILE("assets/settings/1920.png");
@@ -102,16 +102,16 @@ void init_resolution(settings_t *menuv)
     sfVector2f size1_pos = {180, 750};
     sfVector2f size2_pos = {180, 880};
 
-    menuv->res1 = sfSprite_create();
-    sfSprite_setTexture(menuv->res1, res1_texture, sfTrue);
-    sfSprite_setPosition(menuv->res1, res1_pos);
-    menuv->res2 = sfSprite_create();
-    sfSprite_setTexture(menuv->res2, res2_texture, sfTrue);
-    sfSprite_setPosition(menuv->res2, res2_pos);
-    menuv->size1 = sfSprite_create();
-    sfSprite_setTexture(menuv->size1, res1_texture, sfTrue);
-    sfSprite_setPosition(menuv->size1, size1_pos);
-    menuv->size2 = sfSprite_create();
-    sfSprite_setTexture(menuv->size2, res2_texture, sfTrue);
-    sfSprite_setPosition(menuv->size2, size2_pos);
+    menu->res1 = sfSprite_create();
+    sfSprite_setTexture(menu->res1, res1_texture, sfTrue);
+    sfSprite_setPosition(menu->res1, res1_pos);
+    menu->res2 = sfSprite_create();
+    sfSprite_setTexture(menu->res2, res2_texture, sfTrue);
+    sfSprite_setPosition(menu->res2, res2_pos);
+    menu->size1 = sfSprite_create();
+    sfSprite_setTexture(menu->size1, res1_texture, sfTrue);
+    sfSprite_setPosition(menu->size1, size1_pos);
+    menu->size2 = sfSprite_create();
+    sfSprite_setTexture(menu->size2, res2_texture, sfTrue);
+    sfSprite_setPosition(menu->size2, size2_pos);
 }
