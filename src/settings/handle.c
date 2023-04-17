426eca253d20063d034c1642e6da0a21724d8977
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-17 16:46:21 +0200

    🚀 chore: improve code readability and formatting

--- src/settings/handle.c
+++ src/settings/handle.c
@@ -7,33 +7,33 @@
 
 #include "my_rpg.h"
 
-void handle_music(var_t *var, settings_t *menuv, mouse_event_t *mouse)
+void handle_music(var_t *var, settings_t *menu, mouse_event_t *mouse)
 {
     float volume[5] = {0.0f, 25.0f, 50.0f, 75.0f, 100.0f};
 
     for (int i = 0; i < 5; i++) {
-        if (mouse->x > (float)(50 + 125 * i) * mouse->scalx
-        && mouse->x < (float)(100 + 125 * i) * mouse->scalx
-        && mouse->y > 400.0f * mouse->scaly
-        && mouse->y < 440.0f * mouse->scaly) {
-            menuv->jaugem = i;
-            sfMusic_setVolume(var->sound->theme, volume[i]);
+        if (mouse->x > (float)(50 + 125 * i) * mouse->scale_x
+        && mouse->x < (float)(100 + 125 * i) * mouse->scale_x
+        && mouse->y > 400.0f * mouse->scale_y
+        && mouse->y < 440.0f * mouse->scale_y) {
+            menu->gauge_m = i;
+            sfMusic_setVolume(var->sound->music, volume[i]);
             break;
         }
     }
 }
 
-void handle_sound(var_t *var, settings_t *menuv, mouse_event_t *mouse)
+void handle_sound(var_t *var, settings_t *menu, mouse_event_t *mouse)
 {
     float volume[5] = {0.0f, 25.0f, 50.0f, 75.0f, 100.0f};
 
     for (int i = 0; i < 5; i++) {
-        if (mouse->x > (float)(500 + 125 * i) * mouse->scalx
-        && mouse->x < (float)(550 + 125 * i) * mouse->scalx
-        && mouse->y > 400.0f * mouse->scaly
-        && mouse->y < 440.0f * mouse->scaly) {
-            menuv->jauges = i;
-            sfSound_setVolume(var->sound->spear, volume[i]);
+        if (mouse->x > (float)(500 + 125 * i) * mouse->scale_x
+        && mouse->x < (float)(550 + 125 * i) * mouse->scale_x
+        && mouse->y > 400.0f * mouse->scale_y
+        && mouse->y < 440.0f * mouse->scale_y) {
+            menu->gauge_s = i;
+            sfSound_setVolume(SOUND->sound, volume[i]);
             break;
         }
     }
@@ -44,25 +44,25 @@ void handle_res(var_t *var, sfUint32 width, sfUint32 height, sfUint32 style)
     sfVideoMode mode = {width, height, 32};
 
     sfRenderWindow_close(var->window);
-    var->defaultview = sfView_create();
-    sfView_setSize(var->defaultview, (sfVector2f){1920, 1080});
-    sfView_setCenter(var->defaultview, (sfVector2f){1920 / 2, 1080 / 2});
+    var->default_view = sfView_create();
+    sfView_setSize(var->default_view, (sfVector2f){1920, 1080});
+    sfView_setCenter(var->default_view, (sfVector2f){1920 / 2, 1080 / 2});
     var->window = sfRenderWindow_create(mode, "My_RPG", style, NULL);
-    sfRenderWindow_setView(var->window, var->defaultview);
+    sfRenderWindow_setView(var->window, var->default_view);
 }
 
-void choose_resolution(var_t *var, float scalx, float scaly, sfEvent event)
+void choose_resolution(var_t *var, float scalex, float scaley, sfEvent event)
 {
-    if (event.mouseButton.x > 730 * scalx && event.mouseButton.x < 980 * scalx
-    && event.mouseButton.y > 750 * scaly && event.mouseButton.y < 843 * scaly)
+    if (event.mouseButton.x > 730 * scalex && event.mouseButton.x < 980 * scalex
+    && event.mouseButton.y > 750 * scaley && event.mouseButton.y < 843 * scaley)
         handle_res(var, 800, 600, sfFullscreen);
-    if (event.mouseButton.x > 730 * scalx && event.mouseButton.x < 980 * scalx
-    && event.mouseButton.y > 880 * scaly && event.mouseButton.y < 973 * scaly)
+    if (event.mouseButton.x > 730 * scalex && event.mouseButton.x < 980 * scalex
+    && event.mouseButton.y > 880 * scaley && event.mouseButton.y < 973 * scaley)
         handle_res(var, 1920, 1080, sfFullscreen);
-    if (event.mouseButton.x > 180 * scalx && event.mouseButton.x < 430 * scalx
-    && event.mouseButton.y > 750 * scaly && event.mouseButton.y < 843 * scaly)
+    if (event.mouseButton.x > 180 * scalex && event.mouseButton.x < 430 * scalex
+    && event.mouseButton.y > 750 * scaley && event.mouseButton.y < 843 * scaley)
         handle_res(var, 800, 600, sfDefaultStyle);
-    if (event.mouseButton.x > 180 * scalx && event.mouseButton.x < 430 * scalx
-    && event.mouseButton.y > 880 * scaly && event.mouseButton.y < 973 * scaly)
+    if (event.mouseButton.x > 180 * scalex && event.mouseButton.x < 430 * scalex
+    && event.mouseButton.y > 880 * scaley && event.mouseButton.y < 973 * scaley)
         handle_res(var, 1920, 1080, sfDefaultStyle);
 }
