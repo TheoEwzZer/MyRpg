68c06c9de6b9f2cf4d38675b0c5bebb3ad4ed8b1
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-17 11:24:13 +0200

    ✨ feat: initialize BOSSV and BOSSV->boss with textures and positions

--- src/init2.c
+++ src/init2.c
@@ -16,3 +16,18 @@ void init_sound(var_t *var)
     sfSound_setBuffer(SOUND->spear, SOUND->spear_buf);
     sfSound_setVolume(SOUND->spear, 50.0f);
 }
+
+void init_boss(var_t *var)
+{
+    BOSSV->boss->clothes = CREATE_FROM_FILE("assets/pnj/Boss.png");
+    BOSSV->boss->sprite = sfSprite_create();
+    sfSprite_setTexture(BOSSV->boss->sprite, BOSSV->boss->clothes, sfTrue);
+    BOSSV->boss->walk = sfClock_create();
+    sfSprite_setPosition(BOSSV->boss->sprite, (sfVector2f){750.0f, 2800.0f});
+    sfSprite_setScale(BOSSV->boss->sprite, (sfVector2f){0.75f, 0.75f});
+    BOSSV->boss->life = 200;
+    BOSSV->fire = CREATE_FROM_FILE("assets/particle/fireball.png");
+    BOSSV->fireball = sfSprite_create();
+    sfSprite_setTexture(BOSSV->fireball, BOSSV->fire, sfTrue);
+    BOSSV->clock = sfClock_create();
+}
