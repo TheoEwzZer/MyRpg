7cd689d0e2e490a4811e771642ae946a9937455b
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-17 16:54:50 +0200

    🔨 refactor: rename BOSSV to BOSSC and FIREBALL to MAX_FIREBALL 🐛 fix: fix typo in BOSSC declaration ✨ feat: add fireball_t struct and fireball array to boss_t struct 🔨 refactor: rename boss_room to boss_room_texture in boss_t struct 🔨 refactor: rename clothes to texture in char_t struct 🔨 refactor: rename defaultview to default_view in var struct 🔨 refactor: rename menuv to menu and menuv to menu in function parameters 🔨 refactor: rename jaugem to gauge_m and jauges to gauge_s in settings_t struct 🔨 refactor: rename musicsprite to music_sprite and sizesprite to size_sprite

--- include/my_rpg.h
+++ include/my_rpg.h
@@ -27,6 +27,8 @@
     #define SKELETON var->skeleton
     #define PLAYER var->player
     #define BOSSV var->boss
+    #define BOSSC var->boss->boss
+    #define FIREBALL BOSSV->fireball
 
     #define INVENTORY var->inventory
 
@@ -36,6 +38,7 @@
 
     #define MAX_PARTICLES 10
     #define MAX_LEAVES 50
+    #define MAX_FIREBALL 3
 
     #define WIDTH 1920
     #define HEIGHT 1080
@@ -96,17 +99,20 @@ typedef enum direction_e {
 } direction_t;
 
 typedef struct character {
-    sfBool attack;
     direction_t direction;
     float second;
+    float fireball_second;
     int life;
+    sfBool attack;
     sfClock *walk;
+    sfClock *fireball;
     sfIntRect rect;
     sfRectangleShape *hitbox;
     sfRectangleShape *spear;
     sfSprite *sprite;
-    sfTexture *clothes;
+    sfTexture *texture;
     sfTime times;
+    sfTime fireball_t;
 } char_t ;
 
 typedef struct life_s {
@@ -145,9 +151,9 @@ typedef enum quest_e {
 } quest_t;
 
 typedef struct sound_s {
-    sfMusic *theme;
-    sfSound *spear;
-    sfSoundBuffer *spear_buf;
+    sfMusic *music;
+    sfSound *sound;
+    sfSoundBuffer *buffer;
 } sound_t;
 
 typedef struct inventory_s {
@@ -156,68 +162,74 @@ typedef struct inventory_s {
     sfTexture *texture;
 } inventory_t;
 
-typedef struct menuvar {
-    sfSprite *exitsprite;
-    sfSprite *main_menusprite;
-    sfSprite *playsprite;
-    sfSprite *resumesprite;
-    sfSprite *settingsprite;
+typedef struct menu_s {
+    sfSprite *exit_sprite;
+    sfSprite *main_menu_sprite;
+    sfSprite *play_sprite;
+    sfSprite *resume_sprite;
+    sfSprite *setting_sprite;
 } menu_t;
 
-typedef struct settingsvar {
-    int jaugem;
-    int jauges;
-    sfSprite *size2;
-    sfSprite *jaugem0;
-    sfSprite *jaugem1;
-    sfSprite *jaugem2;
-    sfSprite *jaugem3;
-    sfSprite *jaugem4;
-    sfSprite *jauges0;
-    sfSprite *jauges1;
-    sfSprite *jauges2;
-    sfSprite *jauges3;
-    sfSprite *jauges4;
-    sfSprite *menusprite;
-    sfSprite *musicsprite;
-    sfSprite *mutem;
-    sfSprite *mutes;
+typedef struct settings_s {
+    int gauge_m;
+    int gauge_s;
+    sfSprite *menu_sprite;
+    sfSprite *gauge_m0;
+    sfSprite *gauge_m1;
+    sfSprite *gauge_m2;
+    sfSprite *gauge_m3;
+    sfSprite *gauge_m4;
+    sfSprite *music_sprite;
+    sfSprite *mute_music;
+    sfSprite *mute_sound;
     sfSprite *res1;
     sfSprite *res2;
-    sfSprite *resolutionsprite;
+    sfSprite *resolution_sprite;
     sfSprite *size1;
-    sfSprite *sizesprite;
-    sfSprite *soundsprite;
+    sfSprite *size2;
+    sfSprite *size_sprite;
+    sfSprite *gauge_s0;
+    sfSprite *gauge_s1;
+    sfSprite *gauge_s2;
+    sfSprite *gauge_s3;
+    sfSprite *gauge_s4;
+    sfSprite *sound_sprite;
 } settings_t;
 
 typedef struct mouse_event_s {
+    float scale_x;
+    float scale_y;
     int x;
     int y;
-    float scalx;
-    float scaly;
 } mouse_event_t;
 
 typedef struct window_settings_s {
-    sfVector2u size;
-    int width;
     int height;
     int style;
+    int width;
+    sfVector2u size;
 } window_settings_t;
+
+typedef struct fireball_s {
+    sfBool is_active;
+    sfSprite *sprite;
+    sfVector2f direction;
+} fireball_t;
 
 typedef struct boss_s {
     char_t *boss;
-    sfVector2f p_pos;
-    sfVector2f b_pos;
+    fireball_t **fireball;
+    int boss_height;
     int boss_left;
     int boss_top;
-    int boss_height;
     int boss_width;
-    sfTexture *boss_room;
-    sfTexture *map;
-    sfSprite *room;
-    sfSprite *fireball;
-    sfTexture *fire;
     sfClock *clock;
+    sfSprite *room;
+    sfTexture *boss_room;
+    sfTexture *fire;
+    sfTexture *map;
+    sfVector2f b_pos;
+    sfVector2f p_pos;
 } boss_t;
 
 typedef struct var {
@@ -245,7 +257,7 @@ typedef struct var {
     sfSprite **foreground;
     sfSprite *background_sprite;
     sfTexture *armor;
-    sfView *defaultview;
+    sfView *default_view;
     sfView *view;
     size_t frame_count;
     sound_t *sound;
@@ -256,7 +268,8 @@ char *int_to_str(int nb, size_t *n);
 int get_digits(int nb);
 int main(void);
 settings_t *init_settings(void);
-sfBool check_intersects(sfFloatRect p_bounds, var_t *var);
+sfBool check_intersects(sfFloatRect rect, var_t *var);
+sfBool load_player_life(var_t *var, char *line);
 sfBool load_position_map(var_t *var, char *line);
 sfBool load_position_orc(var_t *var, char *line);
 sfBool load_position_player(var_t *var, char *line);
@@ -266,6 +279,7 @@ sfBool load_position_skeleton(var_t *var, char *line);
 sfBool load_quest(var_t *var, char *line);
 sfFloatRect create_enemy_rect(sfVector2f direction, char_t *enemy);
 sfRenderWindow *create_window(void);
+sfVector2f get_fireball_direction(var_t *var);
 void animate_orc(var_t *var);
 void animate_skeleton(var_t *var);
 void attack_down(var_t *var);
@@ -289,7 +303,7 @@ void check_move(var_t *var, sfEvent event);
 void check_move1(var_t *var, sfEvent event, sfFloatRect p_bounds);
 void check_move2(var_t *var, sfEvent event, sfFloatRect p_bounds);
 void check_quest(var_t *var);
-void choose_resolution(var_t *var, float scalx, float scaly, sfEvent event);
+void choose_resolution(var_t *var, float scale_x, float scale_y, sfEvent event);
 void create_barrier_collider(var_t *var);
 void create_collider(var_t *var);
 void create_collider2(var_t *var);
@@ -306,15 +320,15 @@ void create_west_collider(var_t *var);
 void dialog(var_t *var);
 void display_boss(var_t *var);
 void display_life(var_t *var);
-void display_menu(sfRenderWindow *window, menu_t *menuv);
-void display_menu_settings(sfRenderWindow *window, settings_t *menuv);
+void display_menu(sfRenderWindow *window, menu_t *menu);
+void display_menu_settings(sfRenderWindow *window, settings_t *menu);
 void display_orc(var_t *var);
 void display_skeleton(var_t *var);
 void display_ui(var_t *var);
 void down_move(var_t *var);
-void draw_settings(settings_t *menuv, sfRenderWindow *window);
-void event_menu(var_t *var, sfEvent event, menu_t *menuv);
-void event_menu_settings(var_t *var, sfEvent event, settings_t *menuv);
+void draw_settings(settings_t *menu, sfRenderWindow *window);
+void event_menu(var_t *var, sfEvent event, menu_t *menu);
+void event_menu_settings(var_t *var, sfEvent event, settings_t *menu);
 void fight_orc(var_t *var);
 void fight_skeleton(var_t *var);
 void game_engine(var_t *var, sfEvent event);
@@ -322,21 +336,21 @@ void game_engine2(var_t *var);
 void generate_leaves(var_t *var, sfTexture *leaf_texture);
 void generate_particle_pnj(var_t *var, sfVector2f position);
 void girl_move(var_t *var);
-void handle_music(var_t *var, settings_t *menuv, mouse_event_t *mouse);
+void handle_music(var_t *var, settings_t *menu, mouse_event_t *mouse);
 void handle_res(var_t *var, sfUint32 width, sfUint32 height, sfUint32 style);
-void handle_sound(var_t *var, settings_t *menuv, mouse_event_t *mouse);
+void handle_sound(var_t *var, settings_t *menu, mouse_event_t *mouse);
 void init_boss(var_t *var);
 void init_char(var_t *var);
 void init_game(var_t *var);
 void init_life(var_t *var);
-void init_menu(menu_t *menuv);
-void init_mute(settings_t *menuv);
+void init_menu(menu_t *menu);
+void init_mute(settings_t *menu);
 void init_orc(var_t *var);
 void init_player(var_t *var);
-void init_resolution(settings_t *menuv);
+void init_resolution(settings_t *menu);
 void init_rpg(var_t *var);
-void init_settingsthree(settings_t *menuv);
-void init_settingstwo(settings_t *menuv);
+void init_settings_three(settings_t *menu);
+void init_settings_two(settings_t *menu);
 void init_skeleton(var_t *var);
 void init_sound(var_t *var);
 void init_struct(var_t *var);
@@ -350,6 +364,7 @@ void load_inventory(var_t *var);
 void main_menu(var_t *var);
 void map(var_t *var);
 void menu_settings(var_t *var);
+void move_fireball(var_t *var, sfUint32 i);
 void move_leaves(var_t *var);
 void move_particle_pnj(var_t *var);
 void move_particle_position_pnj(var_t *var, sfVector2f position);
@@ -357,11 +372,13 @@ void pnj_move(var_t *var);
 void priscilla_dialog(var_t *var);
 void right_move(var_t *var);
 void save_game(const char *file_name, var_t *var);
+void save_player_life(var_t *var, FILE *file);
 void save_position_map(var_t *var, FILE *file);
 void save_position_orc(var_t *var, FILE *file);
 void save_position_player(var_t *var, FILE *file);
 void save_position_skeleton(var_t *var, FILE *file);
 void save_quest(var_t *var, FILE *file);
+void set_fireball_position(var_t *var);
 void set_foreground_position(var_t *var);
 void show_blacksmith_dialog(var_t *var);
 void show_bob_dialog(var_t *var);
