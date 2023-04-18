b2b07c369456ab89f500f8997491007ad56de5d7
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-18 14:07:24 +0200

    ✨ feat: add boss fight and fireball mechanics

---
+++ src/enemies/boss/boss.c
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2023
+** boss.c
+** File description:
+** boss.c
+*/
+
+#include "my_rpg.h"
+
+void fight_boss(var_t *var)
+{
+    sfFloatRect boss_bounds = sfSprite_getGlobalBounds(BOSSC->sprite);
+    sfFloatRect player_spear = GET_BOUNDS(PLAYER->spear);
+    static sfClock *clock = NULL;
+
+    if (!clock)
+        clock = sfClock_create();
+    if (PLAYER->attack
+    && sfFloatRect_intersects(&player_spear, &boss_bounds, NULL)) {
+        if (sfTime_asSeconds(sfClock_getElapsedTime(clock)) >= 1.0f) {
+            BOSSC->life -= 20;
+            sfClock_restart(clock);
+        }
+    }
+}
+
+void display_boss(var_t *var)
+{
+    BOSSV->boss_left = 470; BOSSV->boss_top = 2650;
+    BOSSV->boss_height = 420; BOSSV->boss_width = 710;
+    BOSSV->b_pos.x = (float)(BOSSV->boss_left + (rand() % BOSSV->boss_width));
+    BOSSV->b_pos.y = (float)(BOSSV->boss_top + (rand() % BOSSV->boss_height));
+    BOSSC->times = sfClock_getElapsedTime(BOSSC->walk);
+    BOSSC->fireball_t = sfClock_getElapsedTime(BOSSC->fireball);
+    BOSSC->second = (float)BOSSC->times.microseconds / 1000000.0f;
+    BOSSC->fireball_second = (float)BOSSC->fireball_t.microseconds / 1000000.0f;
+    if (BOSSC->second > 5.0f) {
+        sfSprite_setPosition(BOSSC->sprite, BOSSV->b_pos);
+        BOSSV->b_pos.x += 20.0f;
+        BOSSV->b_pos.y += 30.0f;
+        set_fireball_position(var);
+        sfClock_restart(BOSSC->walk);
+    }
+    if (BOSSC->fireball_second > 0.01f) {
+        for (sfUint32 i = 0; i < MAX_FIREBALL; i++)
+            move_fireball(var, i);
+        sfClock_restart(BOSSC->fireball);
+    }
+}
+
+void boss_fight(var_t *var)
+{
+    if (BOSSC->life <= 0)
+        return;
+    BOSSC->rect.left = 0;
+    BOSSC->rect.top = 0;
+    BOSSC->rect.width = 77;
+    BOSSC->rect.height = 77;
+    sfSprite_setTextureRect(BOSSC->sprite, BOSSC->rect);
+    display_boss(var);
+    fight_boss(var);
+    DRAW_SPRITE(BOSSC->sprite);
+    check_fireball_collision(var);
+    for (sfUint32 i = 0; i < MAX_FIREBALL; i++) {
+        if (FIREBALL[i]->is_active) {
+            DRAW_SPRITE(FIREBALL[i]->sprite);
+        }
+    }
+}
--- src/enemies/boss/fireball.c
+++ src/enemies/boss/fireball.c
@@ -19,8 +19,10 @@ sfVector2f get_fireball_direction(var_t *var)
     boss_dir.y = player_position.y - boss_position.y;
     distance = sqrtf(powf(boss_dir.x, 2.f) + powf(boss_dir.y, 2.f));
     direction = (sfVector2f){boss_dir.x / distance, boss_dir.y / distance};
-    direction.x *= 1.5f;
-    direction.y *= 1.5f;
+    if (BOSSC->life <= 100) {
+        direction.x *= 1.5f;
+        direction.y *= 1.5f;
+    }
     return direction;
 }
 
@@ -31,6 +33,7 @@ void set_fireball_position(var_t *var)
             FIREBALL[i]->is_active = sfTrue;
             FIREBALL[i]->direction = get_fireball_direction(var);
             sfSprite_setPosition(FIREBALL[i]->sprite, BOSSV->b_pos);
+            sfRectangleShape_setPosition(FIREBALL[i]->hitbox, BOSSV->b_pos);
             break;
         }
     }
@@ -40,6 +43,7 @@ void move_fireball(var_t *var, sfUint32 i)
 {
     if (FIREBALL[i]->is_active) {
         sfSprite_move(FIREBALL[i]->sprite, FIREBALL[i]->direction);
+        sfRectangleShape_move(FIREBALL[i]->hitbox, FIREBALL[i]->direction);
         if (sfSprite_getPosition(FIREBALL[i]->sprite).x < 470
         || sfSprite_getPosition(FIREBALL[i]->sprite).x > 1180
         || sfSprite_getPosition(FIREBALL[i]->sprite).y < 2650
@@ -48,41 +52,17 @@ void move_fireball(var_t *var, sfUint32 i)
     }
 }
 
-void display_boss(var_t *var)
+void check_fireball_collision(var_t *var)
 {
-    BOSSV->boss_left = 470; BOSSV->boss_top = 2650;
-    BOSSV->boss_height = 420; BOSSV->boss_width = 710;
-    BOSSV->b_pos.x = (float)(BOSSV->boss_left + (rand() % BOSSV->boss_width));
-    BOSSV->b_pos.y = (float)(BOSSV->boss_top + (rand() % BOSSV->boss_height));
-    BOSSC->times = sfClock_getElapsedTime(BOSSC->walk);
-    BOSSC->fireball_t = sfClock_getElapsedTime(BOSSC->fireball);
-    BOSSC->second = (float)BOSSC->times.microseconds / 1000000.0f;
-    BOSSC->fireball_second = (float)BOSSC->fireball_t.microseconds / 1000000.0f;
-    if (BOSSC->second > 3.0f) {
-        sfSprite_setPosition(BOSSC->sprite, BOSSV->b_pos);
-        BOSSV->b_pos.x += 20.0f;
-        BOSSV->b_pos.y += 30.0f;
-        set_fireball_position(var);
-        sfClock_restart(BOSSC->walk);
-    }
-    if (BOSSC->fireball_second > 0.01f) {
-        for (sfUint32 i = 0; i < MAX_FIREBALL; i++)
-            move_fireball(var, i);
-        sfClock_restart(BOSSC->fireball);
-    }
-}
+    sfFloatRect player_bounds = GET_BOUNDS(PLAYER->hitbox);
+    sfFloatRect fireball_bounds = {0.0f, 0.0f, 0.0f, 0.0f};
 
-void boss_fight(var_t *var)
-{
-    BOSSC->rect.left = 0;
-    BOSSC->rect.top = 0;
-    BOSSC->rect.width = 77;
-    BOSSC->rect.height = 77;
-    sfSprite_setTextureRect(BOSSC->sprite, BOSSC->rect);
-    display_boss(var);
-    DRAW_SPRITE(BOSSC->sprite);
     for (sfUint32 i = 0; i < MAX_FIREBALL; i++) {
-        if (FIREBALL[i]->is_active)
-            DRAW_SPRITE(FIREBALL[i]->sprite);
+        fireball_bounds = GET_BOUNDS(FIREBALL[i]->hitbox);
+        if (FIREBALL[i]->is_active
+        && sfFloatRect_intersects(&player_bounds, &fireball_bounds, NULL)) {
+            PLAYER->life -= 20;
+            FIREBALL[i]->is_active = sfFalse;
+        }
     }
 }
