cd6e41a4aca49c2969f90dee33a8ddd06a44fe46
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-18 16:32:13 +0200

    ✨ feat(exp): add function to display inventory and exp bar, and update UI display function to call it

--- src/ui.c
+++ src/ui.c
@@ -7,12 +7,55 @@
 
 #include "my_rpg.h"
 
+void check_exp(var_t *var)
+{
+    if (PLAYER->exp == 7)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp7, sfTrue);
+    if (PLAYER->exp == 6)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp6, sfTrue);
+    if (PLAYER->exp == 5)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp5, sfTrue);
+    if (PLAYER->exp == 4)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp4, sfTrue);
+    if (PLAYER->exp == 3)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp3, sfTrue);
+    if (PLAYER->exp == 2)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp2, sfTrue);
+    if (PLAYER->exp == 1)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp1, sfTrue);
+    if (PLAYER->exp == 0)
+        sfSprite_setTexture(INVENTORY->bar, INVENTORY->exp0, sfTrue);
+}
+
+void stat_set_pos(var_t *var, float x, float y)
+{
+    sfVector2f spear_pos = {0.0f, 0.0f};
+    sfVector2f atk_pos = {0.0f, 0.0f};
+    spear_pos.x = x + 125.0f;
+    spear_pos.y = y + 75.0f;
+    atk_pos.x = x + 200.0f;
+    atk_pos.y = y + 73.0f;
+    sfSprite_setPosition(INVENTORY->lance, spear_pos);
+    sfText_setPosition(INVENTORY->atk, atk_pos);
+}
+
+void display_inventory(var_t *var)
+{
+    check_exp(var);
+    if (INVENTORY->is_open) {
+        DRAW_SPRITE(INVENTORY->sprite);
+        DRAW_SPRITE(INVENTORY->bar);
+        DRAW_SPRITE(INVENTORY->lance);
+        sfRenderWindow_drawText(var->window, INVENTORY->atk, NULL);
+    }
+}
+
 void display_ui(var_t *var)
 {
     sfVector2f offset = sfView_getCenter(var->view);
     sfVector2f position_text = {0.0f, 0.0f};
     sfVector2f inventory = {0.0f, 0.0f};
-
+    sfVector2f exp_pos = {0.0f, 0.0f};
     offset.x -= sfView_getSize(var->view).x / 2.0f;
     offset.y -= sfView_getSize(var->view).y / 2.0f;
     position_text.x = offset.x + var->quest_text->position.x;
@@ -21,12 +64,14 @@ void display_ui(var_t *var)
     sfText_setPosition(var->quest_text->text, position_text);
     DRAW_SPRITE(var->quest_text->box);
     sfRenderWindow_drawText(var->window, var->quest_text->text, NULL);
-
     inventory.x = offset.x + 235.0f;
     inventory.y = offset.y + 75.0f;
+    exp_pos.x = inventory.x + 80.0f;
+    exp_pos.y = inventory.y - 20.0f;
     sfSprite_setPosition(INVENTORY->sprite, inventory);
-    if (INVENTORY->is_open)
-        DRAW_SPRITE(INVENTORY->sprite);
+    sfSprite_setPosition(INVENTORY->bar, exp_pos);
+    stat_set_pos(var, inventory.x, inventory.y);
+    display_inventory(var);
 }
 
 void init_ui(var_t *var)
@@ -49,4 +94,5 @@ void init_ui(var_t *var)
     INVENTORY->sprite = sfSprite_create();
     sfSprite_setTexture(INVENTORY->sprite, INVENTORY->texture, sfTrue);
     sfSprite_setScale(INVENTORY->sprite, (sfVector2f){1.5f, 1.5f});
+    init_exp(var);
 }
