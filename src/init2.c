a490a41118ee2efe3e7a09e1ef5198901c159052
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-18 14:05:22 +0200

    🔨 refactor: extract fireball initialization to a separate function ✨ feat: add fireball hitbox initialization

--- src/init2.c
+++ src/init2.c
@@ -17,18 +17,11 @@ void init_sound(var_t *var)
     sfSound_setVolume(SOUND->sound, 50.0f);
 }
 
-void init_boss(var_t *var)
+void init_fireball(var_t *var)
 {
-    BOSSC->texture = CREATE_FROM_FILE("assets/pnj/Boss.png");
-    BOSSC->sprite = sfSprite_create();
-    sfSprite_setTexture(BOSSC->sprite, BOSSC->texture, sfTrue);
-    BOSSC->walk = sfClock_create();
-    BOSSC->fireball = sfClock_create();
-    sfSprite_setPosition(BOSSC->sprite, (sfVector2f){750.0f, 2800.0f});
-    sfSprite_setScale(BOSSC->sprite, (sfVector2f){0.75f, 0.75f});
-    BOSSC->life = 200;
-    BOSSV->fire = CREATE_FROM_FILE("assets/particle/fireball.png");
+    sfVector2f size = {20.0f, 20.0f};
 
+    BOSSC->fireball = sfClock_create();
     FIREBALL = malloc(sizeof(fireball_t *) * MAX_FIREBALL);
     for (sfUint32 i = 0; i < MAX_FIREBALL; i++) {
         FIREBALL[i] = malloc(sizeof(fireball_t));
@@ -36,7 +29,21 @@ void init_boss(var_t *var)
         FIREBALL[i]->sprite = sfSprite_create();
         sfSprite_setTexture(FIREBALL[i]->sprite, BOSSV->fire, sfTrue);
         sfSprite_setScale(FIREBALL[i]->sprite, (sfVector2f){0.10f, 0.10f});
+        FIREBALL[i]->hitbox = sfRectangleShape_create();
+        sfRectangleShape_setSize(FIREBALL[i]->hitbox, size);
     }
+}
 
+void init_boss(var_t *var)
+{
+    BOSSC->texture = CREATE_FROM_FILE("assets/pnj/Boss.png");
+    BOSSC->sprite = sfSprite_create();
+    sfSprite_setTexture(BOSSC->sprite, BOSSC->texture, sfTrue);
+    BOSSC->walk = sfClock_create();
+    sfSprite_setPosition(BOSSC->sprite, (sfVector2f){750.0f, 2800.0f});
+    sfSprite_setScale(BOSSC->sprite, (sfVector2f){0.75f, 0.75f});
+    BOSSC->life = 200;
+    BOSSV->fire = CREATE_FROM_FILE("assets/particle/fireball.png");
+    init_fireball(var);
     BOSSV->clock = sfClock_create();
 }
