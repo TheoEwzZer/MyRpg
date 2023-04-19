82979f5db1c3ae630b060b46a86bed35636e510c
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-19 13:36:49 +0200

    ✨ feat(tutorial): add function to display tutorial text

---
+++ src/tutorial.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** life.c
+** File description:
+** life.c
+*/
+
+#include "my_rpg.h"
+
+void display_tutorial(var_t *var)
+{
+    sfVector2f offset = sfView_getCenter(var->view);
+
+    offset.x -= 100.0f;
+    if (var->quest_progress == ENEMIES)
+        offset.y -= 25.0f;
+    if (var->tutorial->is_active) {
+        sfText_setPosition(var->tutorial->text, offset);
+        sfRenderWindow_drawText(var->window, var->tutorial->text, NULL);
+    }
+}
