40acae7bfde14012e0e070781b77e33dbd65babd
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-20 14:26:04 +0200

    ✨ feat(pause): add support for back button events and hover effect on settings menu 🎨 style(pause): reformat code for better readability

--- src/settings/menu_settings.c
+++ src/settings/menu_settings.c
@@ -7,23 +7,48 @@
 
 #include "my_rpg.h"
 
-void event_menu_settings(var_t *var, sfEvent event, settings_t *menu)
+void settings_hover(settings_t *menu, sfVector2f mouse)
+{
+    sfFloatRect res1rect = sfSprite_getGlobalBounds(menu->res1);
+    sfFloatRect res2rect = sfSprite_getGlobalBounds(menu->res2);
+    sfFloatRect size1rect = sfSprite_getGlobalBounds(menu->size1);
+    sfFloatRect size2rect = sfSprite_getGlobalBounds(menu->size2);
+    if (sfFloatRect_contains(&res1rect, mouse.x, mouse.y))
+        sfSprite_setColor(menu->res1, sfYellow);
+    else
+        sfSprite_setColor(menu->res1, sfWhite);
+    if (sfFloatRect_contains(&res2rect, mouse.x, mouse.y))
+        sfSprite_setColor(menu->res2, sfYellow);
+    else
+        sfSprite_setColor(menu->res2, sfWhite);
+    if (sfFloatRect_contains(&size1rect, mouse.x, mouse.y))
+        sfSprite_setColor(menu->size1, sfYellow);
+    else
+        sfSprite_setColor(menu->size1, sfWhite);
+    if (sfFloatRect_contains(&size2rect, mouse.x, mouse.y))
+        sfSprite_setColor(menu->size2, sfYellow);
+    else
+        sfSprite_setColor(menu->size2, sfWhite);
+}
+
+void event_menu_settings(var_t *var, sfEvent event,
+settings_t *menu, sfBool is_in_game)
 {
     sfVector2u size = sfRenderWindow_getSize(var->window);
-    float scalex = (float)size.x / 1920;
-    float scaley = (float)size.y / 1080;
+    float scalex = (float)size.x / 1920; float scaley = (float)size.y / 1080;
     mouse_event_t *mouse = malloc(sizeof(mouse_event_t));
+    sfVector2i mouse2 = sfMouse_getPositionRenderWindow(var->window);
+    sfVector2f mouse_float = (sfVector2f){(float)mouse2.x, (float)mouse2.y};
 
-    mouse->x = event.mouseButton.x;
-    mouse->y = event.mouseButton.y;
-    mouse->scale_x = scalex;
-    mouse->scale_y = scaley;
+    mouse->x = event.mouseButton.x; mouse->y = event.mouseButton.y;
+    mouse->scale_x = scalex; mouse->scale_y = scaley;
+    settings_hover(menu, mouse_float);
+    if (event.type == 9 && event.mouseButton.button == sfMouseLeft)
+        settings_pressed(menu, mouse_float);
     if (event.type == 0 || (event.type == 5 && sfKeyboard_isKeyPressed(36)))
         sfRenderWindow_close(var->window);
-    if (event.type == 9 && event.mouseButton.button == sfMouseLeft) {
-        if (event.mouseButton.x > 0 && event.mouseButton.x < 150 * scalex &&
-        event.mouseButton.y > 0 * scaley && event.mouseButton.y < 150 * scaley)
-            main_menu(var);
+    if (event.type == 10 && event.mouseButton.button == sfMouseLeft) {
+        handle_back(event, var, is_in_game);
         handle_music(var, menu, mouse);
         handle_sound(var, menu, mouse);
         choose_resolution(var, scalex, scaley, event);
@@ -72,13 +97,13 @@ void display_menu_settings(sfRenderWindow *window, settings_t *menu)
     sfRenderWindow_display(window);
 }
 
-void menu_settings(var_t *var)
+void menu_settings(var_t *var, sfBool is_in_game)
 {
     settings_t *menus = init_settings();
     sfEvent event;
     while (sfRenderWindow_isOpen(var->window)){
         while (sfRenderWindow_pollEvent(var->window, &event))
-            event_menu_settings(var, event, menus);
+            event_menu_settings(var, event, menus, is_in_game);
         display_menu_settings(var->window, menus);
     }
 }
