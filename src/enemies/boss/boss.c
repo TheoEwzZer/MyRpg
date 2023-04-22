04bab72d4296e63a0b54a472e8abc8ef248315c4
Théo Fabiano <theo.fabiano@epitech.eu>
2023-04-23 01:07:09 +0200

    🐛 fix: fix boss knockback when player attacks

--- src/enemies/boss/boss.c
+++ src/enemies/boss/boss.c
@@ -15,11 +15,9 @@ void fight_boss(var_t *var)
 
     if (!clock)
         clock = sfClock_create();
-    if (PLAYER->attack
-    && sfFloatRect_intersects(&player_spear, &boss_bounds, NULL)) {
-        if (sfTime_asSeconds(sfClock_getElapsedTime(clock)) >= 1.0f) {
-            BOSSC->life -= 30;
-            sfClock_restart(clock);
+    if (sfFloatRect_intersects(&player_spear, &boss_bounds, NULL)) {
+        if (PLAYER->attack) {
+            knockback(var, clock, BOSSC);
         }
     }
 }
